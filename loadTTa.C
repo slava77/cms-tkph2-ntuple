8432704abac7fdb6d5168e2f95b4849d1386c43a
Slava Krutelyov <slava77@gmail.com>
2016-02-11 00:04:16 -0800

    ttbar files PU140

---
+++ loadTTa.C
@@ -0,0 +1,13 @@
+void loadTTa(TChain* ch){
+	ch->Add("/Data/cms/tkph2/stublink/15039_TTbar_14TeV_PU140/a/1403EF5B-13A3-E511-A174-0CC47A78A468/ntuple.root");
+	ch->Add("/Data/cms/tkph2/stublink/15039_TTbar_14TeV_PU140/a/4CF86B47-0FA3-E511-BB98-0CC47A4D7662/ntuple.root");
+	ch->Add("/Data/cms/tkph2/stublink/15039_TTbar_14TeV_PU140/a/A81D8C72-1AA3-E511-8D0E-0CC47A78A418/ntuple.root");
+	ch->Add("/Data/cms/tkph2/stublink/15039_TTbar_14TeV_PU140/a/BA6E7CC7-08A3-E511-9575-0CC47A4D767C/ntuple.root");
+	ch->Add("/Data/cms/tkph2/stublink/15039_TTbar_14TeV_PU140/a/C6FE3D91-18A3-E511-B1EF-0CC47A4D765A/ntuple.root");
+	ch->Add("/Data/cms/tkph2/stublink/15039_TTbar_14TeV_PU140/a/CE6CF933-17A3-E511-B9A5-0025905B8586/ntuple.root");
+	ch->Add("/Data/cms/tkph2/stublink/15039_TTbar_14TeV_PU140/a/CEB9D387-14A3-E511-AF6D-0CC47A78A2F6/ntuple.root");
+	ch->Add("/Data/cms/tkph2/stublink/15039_TTbar_14TeV_PU140/a/D0737984-1AA3-E511-AE7E-002618943854/ntuple.root");
+	ch->Add("/Data/cms/tkph2/stublink/15039_TTbar_14TeV_PU140/a/E20AAEA4-17A3-E511-A037-0CC47A4C8E7E/ntuple.root");
+	ch->Add("/Data/cms/tkph2/stublink/15039_TTbar_14TeV_PU140/a/F40CB60F-0EA3-E511-87B1-0CC47A4D7670/ntuple.root");
+	ch->Add("/Data/cms/tkph2/stublink/15039_TTbar_14TeV_PU140/a/F4D76F0D-0EA3-E511-9CE5-0CC47A4D767E/ntuple.root");
+}
