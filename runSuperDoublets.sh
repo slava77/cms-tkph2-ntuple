184d2325147e6930030d3d1f780136bc2dd29ce6
Slava Krutelyov <slava77@gmail.com>
2019-02-22 11:28:17 -0800

    updated run script to use independent spacing for the barrel and endcap; hardcode the D10 (flat layout) input sample

--- runSuperDoublets.sh
+++ runSuperDoublets.sh
@@ -4,12 +4,13 @@ while [ X$# != X0 ] ; do
     case ${1} in
         -n) shift; n=$1; shift;;
         -mm) shift; mm=$1; shift;;
-        -D)  shift; D=$1; shift;;
+        -DB)  shift; DB=$1; shift;;
+        -DE)  shift; DE=$1; shift;;
         -us)  shift; us=$1; shift;;
     esac
 done
 
-echo -e ".L doubletAnalysis.C++ \n .L loadTTa.C \n\
-t9 = new TChain(\"trkTree/tree\"); loadTTa_v2(t9); ScanChainMockSuperDoublets(t9, ${n}, true, ${mm}, ${D}, ${us});\n.qqqq"\
-    | root -l -b >& a_ttbar_stats_mm${mm}_sd${D}_us${us}_ntv2.log
+echo -e ".L doubletAnalysis.C++ \n\
+t9 = new TChain(\"trackingNtuple/tree\"); t9->Add(\"/Data/cms/tkph2/stublink/CMSSW_9_1_0_pre1-tkNtuple/2023PU140/20634.0_TTbar_14TeV+TTbar_14TeV_TuneCUETP8M1_2023D10PU_GenSimHLBeamSpotFull14+DigiFullTriggerPU_2023D10PU+RecoFullGlobalPU_2023D10PU+HARVESTFullGlobalPU_2023D10PU/trackingNtuple.root\"); ScanChainMockSuperDoublets(t9, ${n}, true, ${mm}, ${DB}, ${DE}, ${us}, false, true, true);\n.qqqq"\
+    | root -l -b >& a_ttbar_stats_91X_2023PU140_20634.0_mm${mm}_sd${DB}cm${DE}cm_us${us}.log
 
